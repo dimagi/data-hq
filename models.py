cce4c86f5e70db59e289000c1b44ad6c410c561a
Dan Myung <dmyung@dimagi.com>
2010-08-11 13:02:18 -0400

    adding some changes to auditing model for a more pure view based checking. adding address concepts to carehq patient

--- models.py
+++ models.py
@@ -1,26 +1,40 @@
 from django.db import models
-from tracking.models import Visitor
+from django.utils.translation import ugettext_lazy as _
 import uuid
-from datetime import datetime
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.sessions.models import Session
+from tracking.models import Visitor
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
-from django.contrib.auth.models import User
 
 def make_uuid():
     return uuid.uuid1().hex
 def getdate():
     return datetime.utcnow()
 
-class AuditEvent(models.Model):
-    pass
+class DeepTracking(models.Model):
+    """
+    A persistent session store for the access logging system for your site. 
+    """    
+    visitor = models.ForeignKey(Visitor)
 
+class AuditEvent(models.Model):    
+    id = models.CharField(_('Access guid'), max_length=32, unique=True, default=make_uuid, primary_key=True) #primary_key override?
+    object_type = models.ForeignKey(ContentType, verbose_name='Case linking content type', blank=True, null=True)
+    object_uuid = models.CharField('object_uuid', max_length=32, db_index=True, blank=True, null=True)
+    content_object = generic.GenericForeignKey('object_type', 'object_uuid')
+    
+    user = models.ForeignKey(User)
+    accessed = models.DateTimeField(default = getdate())
+    
 class FieldAccess(models.Model):
     object_type = models.ForeignKey(ContentType, verbose_name='Case linking content type', blank=True, null=True)
     field = models.CharField(max_length=64, blank=True, null=True)
     
     class Meta:
-        unique_together = ('object_type', 'field')
-    
+        unique_together = ('object_type', 'field')    
 
 class ModelAccessLog(models.Model):    
     id = models.CharField(_('Access guid'), max_length=32, unique=True, default=make_uuid, primary_key=True) #primary_key override?
@@ -35,14 +49,5 @@ class ModelAccessLog(models.Model):
     user = models.ForeignKey(User)
     accessed = models.DateTimeField(default = getdate())
 
-#
-#class ModelAuditEvent(models.Model):
-#    session_key = models.CharField(max_length=40)
-#    ip_address = models.CharField(max_length=20)
-#    user = models.ForeignKey(User, null=True)
-#    user_agent = models.CharField(max_length=255)
-#    referrer = models.CharField(max_length=255)
-#    url = models.CharField(max_length=255)
-#    page_views = models.PositiveIntegerField(default=0)
-#    session_start = models.DateTimeField()
-#    last_update = models.DateTimeField()
+
+import signals
