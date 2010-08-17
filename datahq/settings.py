0c99a6f0b86afbfbdc6afe9ec7e64aa3c94ab55c
Rowena <rluk@dimagi.com>
2010-08-17 15:04:39 -0400

    remove trailing references to rapidsms (in comments)

--- datahq/settings.py
+++ datahq/settings.py
@@ -106,9 +106,6 @@ INSTALLED_APPS = DEFAULT_APPS + HQ_APPS
 LOGIN_REDIRECT_URL='/'
 
 
-# CZUE:  these have been added as new custom properties to the settings
-# and are migrated from the previous rapidsms framework
-
 ####### Receiver Settings #######
 RECEIVER_SUBMISSION_PATH="data/submissions"
 RECEIVER_ATTACHMENT_PATH="data/attachments"
@@ -118,7 +115,6 @@ RECEIVER_EXPORT_PATH="data"
 XFORMMANAGER_SCHEMA_PATH="data/schemas"
 XFORMMANAGER_EXPORT_PATH="data"
 XFORMMANAGER_FORM_TRANSLATE_JAR="lib/form_translate.jar"
-
 
 ####### Domain settings  #######
 
